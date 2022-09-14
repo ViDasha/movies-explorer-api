f24189b676b8bc5629c7ead55db05b2ec23f1a79
Daria Vinogradova <vinodarya@yandex.ru>
2022-09-14 17:55:02 +0300

    fix: delete avatar validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -39,7 +39,6 @@ module.exports.validateSignIn = celebrate({
 
 module.exports.validateSignUp = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(patternURL),
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
